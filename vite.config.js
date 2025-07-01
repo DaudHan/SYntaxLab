0fea19a6541a9c6c9f6d0e0b92363c6041dad558
DaudHan <98137747+DaudHan@users.noreply.github.com>
2025-07-01 21:42:02 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+     server: { // Tambahkan atau pastikan bagian ini ada
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
